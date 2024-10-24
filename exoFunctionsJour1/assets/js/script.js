4d0ba20b69fcd3ceb2ef8090726651416be17c34
Kev <kevin@MacBook-Pro-de-Kev.local>
2024-10-24 16:37:17 +0200

    javascript day1

---
+++ exoFunctionsJour1/assets/js/script.js
@@ -0,0 +1,45 @@
+// Exercice 1 : Ecrire une fonction : Multiplication
+
+// function multiply(a, b) {
+//     return (a * b)
+// }
+// console.log(multiply(3, 10));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Exercice 2 : Optimiser le code : Vérification de parité
+
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true;
+//     } return 'false';
+// }
+// console.log(isEven(10));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Exercice 3 : Ecrire une fonction : Calculer la somme des nombres jusqu'à n
+
+
+// function sumUpTo (num) {
+//     let sum = 0;
+//     for (let i = 0; i<=num; i++) {
+//         sum = sum + i;
+//     } return sum;
+// }
+
+// console.log(sumUpTo(10));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Exercice 4 : Corriger l'erreur 
+// Ajout de la ligne return product -> si on veut garder le let product
+
+// function multiply(a, b) {
+//     let product = a * b;
+//     return product;
+// }
+// console.log(multiply(2, 3));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
